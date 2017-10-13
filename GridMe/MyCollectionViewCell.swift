42f21d09635ce526738df3facc648ad5a6153f4f
sathe <sathe@Sathes-MacBook-Pro.local>
2017-10-13 23:06:48 +0100

    Initial commit

---
+++ GridMe/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  GridMe
+//
+//  Created by sathe on 09/10/17.
+//  Copyright © 2017 Sathe. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
